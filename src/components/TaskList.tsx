5845ce5cba7a1bfb3a0b1e2b728bdda7176006b9
MarcosGaiotto <marcosgaiotto@hotmail.com>
2021-03-15 03:37:06 -0300

    funções  de controle de tasks adicionadas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle) {
+      const newTask : Task = {
+        id: Math.floor(Math.random() * 100),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete ? task.isComplete = false : task.isComplete = true;
+      }
+      return task;
+    }))
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  function handleRemoveTask(id: number) { 
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
